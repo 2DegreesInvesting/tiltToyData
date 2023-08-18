90ea355e082239d334a94fe338406b736bca9b99
Mauro Lepore <maurolepore@gmail.com>
2023-08-18 15:14:05 +0000

    Update docs

--- R/toy_datasets.R
+++ R/toy_datasets.R
@@ -1,9 +1,8 @@
-
 #' Get path to toy datasets
 #'
 #' This package comes bundled with a number of toy datasets in its
-#' 'inst/extdata' directory. Use toy_datasets() to list all the available
-#' datasets and toy_dataset() to retrieve the path to one example.
+#' 'inst/extdata' directory. Use `toy_datasets()` to list all the available
+#' datasets and `toy_dataset()` to retrieve the path to one example.
 #'
 #' @param file File.
 #' @param pattern A regular expression of filenames to match. If `NULL` all
