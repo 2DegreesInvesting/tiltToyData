fefe0168ad1a1c0114659a34057f5adaffcd321a
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 12:43:36 +0000

    wip

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,10 +3,11 @@
 #' @description
 #' `r lifecycle::badge('deprecated')`
 #'
-#' In all `toy_*companies()` the column `company_id` is now deprecated and
-#' replaced with `companies_id`. The current data will be available to you
-#' automatically when you update the tiltIndicator package.
+#' The column `company_id` is now deprecated in all `toy_*companies()`. The
+#' latest data uses `companies_id` and is available to you automatically when
+#' you update the tiltIndicator package.
 #'
 #' @keywords internal
 #' @name deprecated
 #' @aliases NULL
+NULL
--- R/tiltToyData-package.R
+++ R/tiltToyData-package.R
@@ -2,7 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom lifecycle deprecated
 #' @importFrom readr read_csv
+#' @importFrom rlang warn
+#' @importFrom utils packageVersion
 #' @importFrom withr local_options
 ## usethis namespace: end
 NULL
