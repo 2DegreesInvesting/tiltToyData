17ea72b0d0f3fc181b04216d4f095eb1314f1c73
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 12:37:08 +0000

    Document deprecated

---
+++ R/deprecated.R
@@ -0,0 +1,12 @@
+#' Deprecated
+#'
+#' @description
+#' `r lifecycle::badge('deprecated')`
+#'
+#' In all `toy_*companies()` the column `company_id` is now deprecated and
+#' replaced with `companies_id`. The current data will be available to you
+#' automatically when you update the tiltIndicator package.
+#'
+#' @keywords internal
+#' @name deprecated
+#' @aliases NULL
