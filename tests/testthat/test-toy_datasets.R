68bfd18333cea6d8267c4ab56781787ce3b1ff2b
Mauro Lepore <maurolepore@gmail.com>
2024-01-09 19:23:00 +0000

    Make it easier to see problematic snapshots

--- tests/testthat/test-toy_datasets.R
+++ tests/testthat/test-toy_datasets.R
@@ -1,52 +1,52 @@
 test_that("emissions_profile_any_companies hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_emissions_profile_any_companies())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("emissions_profile_products_ecoinvent hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_emissions_profile_products_ecoinvent())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("emissions_profile_upstream_products_ecoinvent hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_emissions_profile_upstream_products_ecoinvent())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("emissions_profile_products hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_emissions_profile_products())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("emissions_profile_upstream_products hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_emissions_profile_upstream_products())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 test_that("sector_profile_any_scenarios hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_sector_profile_any_scenarios())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("sector_profile_companies hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_sector_profile_companies())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("sector_profile_upstream_companies hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_sector_profile_upstream_companies())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
 
 test_that("sector_profile_upstream_products hasn't changed", {
   local_snapshot()
   data <- read_csv(toy_sector_profile_upstream_products())
-  expect_snapshot(as.data.frame(data))
+  expect_snapshot(format_robust_snapshot(data))
 })
