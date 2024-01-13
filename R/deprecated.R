a42cbff4d40b5a51c639988814249fad60223e66
Mauro Lepore <maurolepore@gmail.com>
2024-01-13 15:17:40 +0000

    docs

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,14 +3,14 @@
 #' @description
 #' `r lifecycle::badge('deprecated')`
 #'
+#' - The column `company_id` is now deprecated in all `toy_*companies()`. The
+#' latest data uses `companies_id` and is available to you automatically when
+#' you update the tiltIndicator package.
+#'
 #' - `toy_emissions_profile_products()` and
 #' `toy_emissions_profile_upstream_products()` are deprecated in favor of
 #' `toy_emissions_profile_products_ecoinvent()` and
 #' `toy_emissions_profile_upstream_products_ecoinvent()`, respectively.
-#'
-#' - The column `company_id` is now deprecated in all `toy_*companies()`. The
-#' latest data uses `companies_id` and is available to you automatically when
-#' you update the tiltIndicator package.
 #'
 #' @keywords internal
 #' @name deprecated
