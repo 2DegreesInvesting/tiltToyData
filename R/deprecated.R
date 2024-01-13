9bb00c45d28551d7c843c26ce2a08eff00d52228
Mauro Lepore <maurolepore@gmail.com>
2024-01-13 15:07:48 +0000

    docs

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,22 +3,18 @@
 #' @description
 #' `r lifecycle::badge('deprecated')`
 #'
-#' The column `company_id` is now deprecated in all `toy_*companies()`. The
+#' - `toy_emissions_profile_products()` and
+#' `toy_emissions_profile_upstream_products()` are deprecated in favor of
+#' `toy_emissions_profile_products_ecoinvent()` and
+#' `toy_emissions_profile_upstream_products_ecoinvent()`, respectively.
+#'
+#' - The column `company_id` is now deprecated in all `toy_*companies()`. The
 #' latest data uses `companies_id` and is available to you automatically when
 #' you update the tiltIndicator package.
 #'
 #' @keywords internal
 #' @name deprecated
 #' @aliases NULL
-NULL
-
-#' Deprecated `toy_emissions_profile*_products()`
-#'
-#' @description
-#' `r lifecycle::badge("deprecated")`
-#'
-#' @export
-#' @keywords internal
 #' @examples
 #' toy_emissions_profile_products()
 #' # ->
@@ -27,12 +23,17 @@ NULL
 #' toy_emissions_profile_upstream_products()
 #' # ->
 #' toy_emissions_profile_upstream_products_ecoinvent()
+NULL
+
+#' @rdname deprecated
+#' @export
+#' @keywords internal
 toy_emissions_profile_products <- function() {
   lifecycle::deprecate_warn("0.0.0.9009", "toy_emissions_profile_products()", "toy_emissions_profile_products_ecoinvent()")
   deprecated_path("emissions_profile_products.csv.gz")
 }
 
-#' @rdname toy_emissions_profile_products
+#' @rdname deprecated
 #' @export
 #' @keywords internal
 toy_emissions_profile_upstream_products <- function() {
