91156c308de9977911781c395107695f43156e40
Mauro Lepore <maurolepore@gmail.com>
2024-01-15 11:53:30 -0300

    * New toy datasets from tiltIndicatorAfter v0.0.0.9016 (#30)

--- R/toy_datasets.R
+++ R/toy_datasets.R
@@ -14,6 +14,16 @@
 #' toy_sector_profile_upstream_companies()
 #'
 #' toy_sector_profile_upstream_products()
+#'
+#' toy_europages_companies()
+#'
+#' toy_ecoinvent_activities()
+#'
+#' toy_ecoinvent_inputs()
+#'
+#' toy_ecoinvent_europages()
+#'
+#' toy_isic_name()
 toy_emissions_profile_any_companies <- function() {
   compatible_companies_path(toy_file("emissions_profile_any_companies"))
 }
@@ -52,6 +62,36 @@ toy_sector_profile_upstream_companies <- function() {
 #' @export
 toy_sector_profile_upstream_products <- function() {
   toy_path(toy_file("sector_profile_upstream_products"))
+}
+
+#' @rdname toy_files
+#' @export
+toy_europages_companies <- function() {
+  toy_path(toy_file("europages_companies"))
+}
+
+#' @rdname toy_files
+#' @export
+toy_ecoinvent_activities <- function() {
+  toy_path(toy_file("ecoinvent_activities"))
+}
+
+#' @rdname toy_files
+#' @export
+toy_ecoinvent_inputs <- function() {
+  toy_path(toy_file("ecoinvent_inputs"))
+}
+
+#' @rdname toy_files
+#' @export
+toy_ecoinvent_europages <- function() {
+  toy_path(toy_file("ecoinvent_europages"))
+}
+
+#' @rdname toy_files
+#' @export
+toy_isic_name <- function() {
+  toy_path(toy_file("isic_name"))
 }
 
 toy_file <- function(x) {
